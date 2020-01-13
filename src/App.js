f0d00bca67cf8c56de5476fbf07929245a0f6f2c
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-01-13 15:01:17 -0500

    local storage implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,109 @@
-import React from 'react';
+import React from "react";
+
+import TodoForm from "./components/TodoComponents/TodoForm";
+import Todo from "./components/TodoComponents/Todo";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todoList: [
+        {
+          task: "Organize Garage",
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: "Bake Cookies",
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    const todoList = JSON.parse(localStorage.getItem("todoList"));
+    console.log(todoList);
+    if (todoList) {
+      this.setState({ todoList, loading: false });
+    } else {
+      this.setState({
+        todoList: [
+          {
+            task: "Organize Garage",
+            id: 1528817077286,
+            completed: false
+          },
+          {
+            task: "Bake Cookies",
+            id: 1528817084358,
+            completed: false
+          }
+        ],
+        loading: false
+      });
+    }
+  }
+
+  componentDidUpdate() {
+    this.setLocalStorage();
+  }
+
+  createTodo = todoObject => {
+    this.setState(prevState => ({
+      todoList: [...prevState.todoList, todoObject]
+    }));
+  };
+
+  updateTodo = todoObject => {
+    const updatedTodoObject = {
+      ...todoObject,
+      completed: !todoObject.completed
+    };
+
+    this.setState(prevState => {
+      const newState = prevState.todoList.filter(todoObject => {
+        return todoObject.id !== updatedTodoObject.id;
+      });
+      return { todoList: [...newState, updatedTodoObject] };
+    });
+  };
+
+  clearCompleted = () => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.filter(todoObject => !todoObject.completed)
+    }));
+  };
+
+  setLocalStorage = () => {
+    localStorage.clear();
+    localStorage.setItem("todoList", JSON.stringify(this.state.todoList));
+  };
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+
+        {!this.state.loading && (
+          <div>
+            <TodoForm
+              createTodo={this.createTodo}
+              clearCompleted={this.clearCompleted}
+            />
+            {this.state.todoList.map(todoObject => {
+              return (
+                <Todo
+                  todoObject={todoObject}
+                  handleClick={this.updateTodo}
+                  key={todoObject.id}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
